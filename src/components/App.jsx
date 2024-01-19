90ebb794309356b84ee8de4eec13018f6f35e198
Yuliia <djudit@ukr.net>
2024-01-20 00:26:48 +0200

    lerning css lesson 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Product } from './Product';
+import './App.css';
 
 export default function App() {
   return (
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,8 +1,18 @@
+import './Books.css';
+import { FcBiohazard } from 'react-icons/fc';
+import { ImAirplane } from 'react-icons/im';
+
 export const BookList = ({ books }) => {
   return (
     <ul>
       {books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li key={book.id}>
+            <ImAirplane className="icon-plane" size="60" />
+            <FcBiohazard className="my-icon" size="24" />
+            {book.name}
+          </li>
+        );
       })}
     </ul>
   );
